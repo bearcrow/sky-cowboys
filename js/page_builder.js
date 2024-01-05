c926f469f9acd88d4bf04799dca9cd62bbba4510
root <root@DESKTOP-CHRKOQQ.localdomain>
2024-01-05 08:13:15 -0800

    ammending dossier page

--- js/page_builder.js
+++ js/page_builder.js
@@ -1,17 +1,21 @@
 
 async function include(url, destination){
-	const response = await fetch(url);
-	const text = await response.text();
-	destination.innerHTML = text;
+  const response = await fetch(url);
+  const text = await response.text();
+  destination.innerHTML = text;
 }
 customElements.define("header-component", class extends HTMLElement {
   connectedCallback() {
-	include("/header.html",this);
+  include("/header.html",this);
   }
 });
-
+customElements.define("site-nav", class extends HTMLElement {
+  connectedCallback() {
+  include("/site_nav.html",this);
+  }
+});
 customElements.define("footer-component", class extends HTMLElement {
   connectedCallback() {
-	include("/footer.html",this);
+  include("/footer.html",this);
   }
 });
